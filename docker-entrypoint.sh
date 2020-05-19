e7a17f25417a7f42ebfb3c31c45df5e6ca7eab45
David Tabachnikov <david@tabachnikov.io>
2020-05-19 17:45:39 +0200

    Fix if logic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,11 @@
 if [ ! -z "$GOOGLE_APPLICATION_CREDENTIALS" ] && [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]
 then
     gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+    
     if [ ! -z "CLOUDSDK_CONTAINER_CLUSTER"]
-    gcloud container clusters get-credentials $CLOUDSDK_CONTAINER_CLUSTER
+    then
+        gcloud container clusters get-credentials $CLOUDSDK_CONTAINER_CLUSTER
+    if
 else
     echo "GCP Credentials not found"
 fi
