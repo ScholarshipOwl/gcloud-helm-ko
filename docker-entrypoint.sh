7360b86a297b7c28d03c61d79a2182412855fa08
David Tabachnikov <david@tabachnikov.io>
2020-05-20 09:23:08 +0200

    Fix handling of GCP credentials in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-if [ ! -z "$GOOGLE_APPLICATION_CREDENTIALS" ] && [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]
+# Workaround for GitLab ENTRYPOINT double execution (issue: 1380)
+[ -f /tmp/gitlab-runner.lock ] && exit || >/tmp/gitlab-runner.lock
+
+# If we have a GOOGLE_APPLICATION_CREDENTIALS file defined - activate it
+if [ ! -z "$GOOGLE_APPLICATION_CREDENTIALS" ]
 then
+    if [ ! -f "$GOOGLE_APPLICATION_CREDENTIALS" ]
+    then
+        FILENAME=$(tempfile).json
+        printenv GOOGLE_APPLICATION_CREDENTIALS > "$FILENAME"
+        GOOGLE_APPLICATION_CREDENTIALS="$FILENAME"
+    fi
     gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
     
-    if [ ! -z "CLOUDSDK_CONTAINER_CLUSTER"]
+    if [ ! -z "CLOUDSDK_CONTAINER_CLUSTER" ]
     then
         gcloud container clusters get-credentials $CLOUDSDK_CONTAINER_CLUSTER
     fi
